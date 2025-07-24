27bd4f529d62d78cecaf52fedbd10da38c94e2d4
anthonyb8 <anthonybaxter819@gmail.com>
2025-07-24 15:47:01 -0400

    Updated installation script.

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,28 +2,33 @@
 
 set -e
 FORGE_DIR="$HOME/.forge"
+REPO_URL="https://github.com/anthonyb8/forge.git"
 
 if [ -d "$FORGE_DIR/.git" ]; then
-	echo "Updating forge..."
-	git -C "$FORGE_DIR" pull
+  echo "Updating forge..."
+  git -C "$FORGE_DIR" pull
 else
-	echo "Cloning forge..."
+  echo "Cloning forge..."
+  rm -rf "$FORGE_DIR" # clean out any old directories
+  git clone "$REPO_URL" "$FORGE_DIR"
 fi
 
 if cd "$FORGE_DIR"; then
-	echo "Building forge..."
-	cargo build --release
+  echo "Building forge..."
+  cargo build --release
 
-	echo "Installing forge..."
-	mkdir -p ~/.forge/bin
-	cp target/release/forge ~/.forge/bin/
+  echo "Installing forge..."
+  mkdir -p ~/.forge/bin
+  cp target/release/forge ~/.forge/bin/
 
-	echo ""
-	echo "To use 'forge' from anywhere, add this to your shell config:"
-	echo ""
-	echo "    export PATH=\"\$HOME/.forge/bin:\$PATH\""
-	echo ""
-	echo "Then restart your shell"
+  echo ""
+  echo "To use 'forge' from anywhere, add this to your shell config:"
+  echo ""
+  echo "    export PATH=\"\$HOME/.forge/bin:\$PATH\""
+  echo "or run: "
+  echo "    echo 'export PATH=\"\$HOME/.forge/bin:\$PATH\"' >> ~/.zshrc "
+  echo ""
+  echo "Then restart your shell"
 else
-	echo "Error finding forge directory"
+  echo "Error finding forge directory"
 fi
